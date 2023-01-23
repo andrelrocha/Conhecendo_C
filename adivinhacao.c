db3e3c2964871d721759111b865a024c09ea765d
andrelrocha <andrerocha0911@gmail.com>
2023-01-23 15:57:34 -0300

    scanf e str concatenation

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main () {
-    printf("******************************************\n");
-    printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
-    printf("******************************************\n");
+int main()
+{
+    printf("********************************\n");
+    printf("* Welcome to the guessing game *\n");
+    printf("********************************\n");
+
+    int numeroSecreto = 13;
+
+    int chuteUsuario;
+
+    printf("What's your guess? ");
+    scanf("%d", &chuteUsuario);
+    printf("Your guess was %d", chuteUsuario);
+
 }
