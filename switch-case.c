99d37cf3115987f273020606d1024ade0de713c7
andrelrocha <andrerocha0911@gmail.com>
2023-01-25 09:30:48 -0300

    switch-case statement in C

---
+++ switch-case.c
@@ -0,0 +1,11 @@
+switch (expression) {
+    case value1:
+        // code to be executed if expression == value1
+        break;
+    case value2:
+        // code to be executed if expression == value2
+        break;
+    ...
+    default:
+        // code to be executed if expression does not match any case
+}
