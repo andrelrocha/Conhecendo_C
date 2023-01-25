b07fd234a603b1e6eabcb3e387ac0cd42abcf764
andrelrocha <andrerocha0911@gmail.com>
2023-01-25 09:43:41 -0300

    adicionando estrutura C e exemplo com switch-case

--- switch-case.c
+++ switch-case.c
@@ -1,3 +1,4 @@
+/*
 switch (expression) {
     case value1:
         // code to be executed if expression == value1
@@ -9,3 +10,41 @@ switch (expression) {
     default:
         // code to be executed if expression does not match any case
 }
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int numerodetentativas;
+    int nivel;
+
+    printf("Qual o nivel de dificuldade?\n");
+    printf("(1) Facil (2) Medio (3) Dificil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    switch(nivel) {
+        case 1:
+            numerodetentativas = 20;
+            break;
+        case 2:
+            numerodetentativas = 15;
+            break;
+        default:
+            numerodetentativas = 6;
+    }
+
+    printf(numerodetentativas);
+}
+
+/*
+    if(nivel == 1) {
+        numerodetentativas = 20;
+    }
+    else if (nivel == 2) {
+        numerodetentativas = 15;
+    }
+    else {
+        numerodetentativas = 6;
+    }
+*/
